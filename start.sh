b9650adafd860dd179c7c0a79f18af121462fd39
markhuang1994 <markhuang1993@gmail.com>
2020-03-07 03:21:19 +0800

    optimise

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 [[ ! -f ./pom_dir ]] && cp pom.xml ./pom_dir
-mvn -f ./pom_dir clean
-mvn -f ./pom_dir -Dspring-boot.run.jvmArguments=-Dspring.config.location=/var/opt/ssl/data/application-docker.yml spring-boot:run
+mvn clean
+mvn -Dspring-boot.run.jvmArguments=-Dspring.config.location=/var/opt/ssl/data/application-docker.yml spring-boot:run
