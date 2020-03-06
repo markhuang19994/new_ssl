7625f20e9dd58d2b1d3b18286f387c03296de1a8
markhuang1994 <markhuang1993@gmail.com>
2020-03-07 04:08:19 +0800

    use volume pom.xml

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-[[ ! -f ./pom_dir ]] && cp pom.xml ./pom_dir
+[[ ! -f ./pom_dir/pom.xml ]] && cp pom.xml ./pom_dir
+rm -rf pom.xml && cp ./pom_dir/pom.xml .
 mvn clean
 mvn -Dspring-boot.run.jvmArguments=-Dspring.config.location=/var/opt/ssl/data/application-docker.yml spring-boot:run
