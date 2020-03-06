39c3c5db527ec1991c3f79f3e7e51b679206f4f4
markhuang1994 <markhuang1993@gmail.com>
2020-03-07 03:28:52 +0800

    add head declare

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 [[ ! -f ./pom_dir ]] && cp pom.xml ./pom_dir
 mvn clean
 mvn -Dspring-boot.run.jvmArguments=-Dspring.config.location=/var/opt/ssl/data/application-docker.yml spring-boot:run
