523307528ee8fd8c659baf1cb2c0d286ba055722
markhuang1994 <markhuang1993@gmail.com>
2020-03-11 15:33:11 +0800

    update resources copy path

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-/bin/cp -fR /usr/local/docker/volume/com /usr/local/project/new_ssl/src/main/java/com
-/bin/cp -fR /usr/local/docker/volume/data /var/opt/ssl/data
-/bin/cp -fR /usr/local/docker/volume/pom /usr/local/project/new_ssl/pom_dir
+/bin/cp -fR /usr/local/docker/dummy_api/volume/com /usr/local/project/new_ssl/src/main/java/com
+/bin/cp -fR /usr/local/docker/dummy_api/volume/data /var/opt/ssl/data
+/bin/cp -fR /usr/local/docker/dummy_api/volume/pom/pom.xml /usr/local/project/new_ssl/pom.xml
 
 mvn clean
 mvn -Dspring-boot.run.jvmArguments=-Dspring.config.location=/var/opt/ssl/data/application-docker.yml spring-boot:run
