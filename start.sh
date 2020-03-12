724305d29d23602204839ad066f8fcda85ffa90e
markhuang1994 <markhuang1993@gmail.com>
2020-03-12 11:42:43 +0800

    optimise

--- start.sh
+++ start.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 if [ "$1" = 'first' ]; then
   echo 'start...'
+  rm -f /tmp/myLog.log
+  cd /usr/local/docker/dummy_api || exit
+  git fetch --progress origin master
+  git clean -f
+  git checkout -f origin/master
 else
   echo 'restart...'
   sleep 5
@@ -17,9 +22,8 @@ mkdir -p /var/opt/ssl/data
 rm -f /usr/local/project/new_ssl/pom.xml
 /bin/cp -f /usr/local/docker/dummy_api/volume/pom/pom.xml /usr/local/project/new_ssl/pom.xml
 
-rm -f /tmp/myLog.log
+cd /usr/local/project/new_ssl || exit
 mvn clean
-
 spring_jvm_args='-Dspring-boot.run.jvmArguments=-Dspring.config.location=/var/opt/ssl/data/application-docker.yml'
 if [ "$1" = 'first' ]; then
   mvn "$spring_jvm_args" spring-boot:run >/dev/null 2>&1 &
